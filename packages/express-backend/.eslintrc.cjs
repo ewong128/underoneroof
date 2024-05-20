d59fda77314a5675910618b31c4de990170e26bd
Reva Moolky <>
2024-05-20 14:08:08 -0700

    prettier + lint

---
+++ packages/express-backend/.eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
+
+}
