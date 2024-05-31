b7a24f659fa6e1954f2609784bfc965166a2ded5
ewong128 <ewong128@calpoly.edu>
2024-05-31 12:31:44 -0700

    Add some details

--- packages/react-frontend/src/routes/EventTable.jsx
+++ packages/react-frontend/src/routes/EventTable.jsx
@@ -23,7 +23,6 @@ function TableHeader() {
 
 function TableBody(props) {
   if (props.eventData === null) {
-  if (props.eventData === null) {
     return <caption>Data Unavailable</caption>;
   }
 
@@ -36,25 +35,24 @@ function TableBody(props) {
       <td>{row.name}</td>
       <td>{row.event}</td>
       <td>{row.description}</td>
-      <td>
       <td style={{ textAlign: "center" }}>
-          <button
-            onClick={() => props.removeEvent(index)}
-            style={{
-              padding: 0,
-              border: "none",
-              background: "none",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center"
-            }}
-          >
-            <img
-              src={trashDelete}
-              alt="Delete"
-              style={{ width: "20px", height: "25px", marginRight: "-45px", marginTop: "5px" }}
-            />
-          </button>
+        <button
+          onClick={() => props.removeEvent(index)}
+          style={{
+            padding: 0,
+            border: "none",
+            background: "none",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <img
+            src={trashDelete}
+            alt="Delete"
+            style={{ width: "20px", height: "25px" }}
+          />
+        </button>
       </td>
     </tr>
   ));
@@ -75,63 +73,3 @@ function EventTable(props) {
 }
 
 export default EventTable;
-
-// // src/Table.jsx
-// import React from "react";
-
-// function TableHeader() {
-//   return (
-//     <thead>
-//       <tr>
-//         <th>Date</th>
-// 		<th>Time</th>
-//         <th>Name</th>
-//         <th>Events</th>
-// 		<th>Description</th>
-//         <th>Delete</th>
-//       </tr>
-//     </thead>
-//   );
-// }
-
-// function TableBody(props) {
-//     if(props.eventData === null) {
-//     return <caption>Data Unavailable</caption>;
-//     }
-//   const rows = props.eventData.map((row, index) => {
-//     return(
-//       <tr key = {index}>
-//         <td>{row.date}</td>
-// 		<td>{row.time}</td>
-//         <td>{row.name}</td>
-//         <td>{row.event}</td>
-// 		<td>{row.description}</td>
-//         <td>
-//           <button onClick={() => props.removeEvent(index)}>
-//             Delete
-//           </button>
-//         </td>
-//       </tr>
-//     )
-//   }
-//   )
-//   return(
-//     <tbody>
-//       {rows}
-//     </tbody>
-//   );
-// }
-
-// function EventTable(props) {
-//   return (
-//     <table>
-//       <TableHeader />
-//       <TableBody 
-//       eventData = {props.eventData}
-//       removeEvent = {props.removeEvents}
-//       />
-//     </table>
-//   );
-// }
-
-// export default EventTable;
